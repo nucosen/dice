e98eee8ef2209e58abbd9015d6e6101f8e0bab08
kotani-py <kotani.takumi@g.wakayama-u.jp>
2022-01-01 12:34:18 +0900

    おみくじを追加

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dice",
-    version='4.0.0',
+    version='4.1.0',
     description='Discord dice bot',
     author='NUCOSen Management Committee',
     author_email='info@nucosen.live',
