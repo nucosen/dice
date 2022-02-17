e04a69f4b2ed930a718d04c84337b0892b792b0a
NUCO <93378833+sitting-cat@users.noreply.github.com>
2022-02-17 10:26:04 +0900

    軽微な修正

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="dice",
-    version='5.1.3',
+    version='5.1.4',
     description='Discord dice bot',
     author='NUCOSen Management Committee',
     author_email='info@nucosen.live',
     url='https://github.com/nucosen/dice',
     packages=find_packages(),
-    entry_points="""
-      [console_scripts]
-      dice = dice.cli:execute
-    """,
+    entry_points={
+      'console_scripts':[
+        'dice = dice.cli:execute'
+      ]
+    },
     install_requires=open(
         'requirements.txt', encoding="utf-16"
     ).read().splitlines(),
