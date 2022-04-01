bef35976f9079983174ee37427fada810a393fc3
NUCO <93378833+sitting-cat@users.noreply.github.com>
2022-04-01 12:57:15 +0900

    再デプロイに関係するバージョン番号の変更

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dice",
-    version='5.2.0',
+    version='5.2.1',
     description='Discord dice bot',
     author='NUCOSen Management Committee',
     author_email='info@nucosen.live',
