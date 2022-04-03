f000774e5d4881167874206e0b65db43c8cf1723
kotani-py <kotani.takumi@g.wakayama-u.jp>
2022-04-03 15:49:31 +0900

    バージョン番号の調声

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # NOTE : Version here
 setup(
     name="dice",
-    version='6.0.1b3',
+    version='6.0.1',
     description='Discord dice bot',
     author='NUCOSen Management Committee',
     author_email='info@nucosen.live',
